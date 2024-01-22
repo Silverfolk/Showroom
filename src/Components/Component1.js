dc5ea81b0a79d6a63fed9f6ccfec724afc4a5a99
Silverfolk <kharsh822@gmail.com>
2024-01-22 14:47:36 +0530

    prettify code

--- src/Components/Component1.js
+++ src/Components/Component1.js
@@ -1,51 +1,50 @@
-import React from 'react'
+import React from 'react';
 import Phone from '../Utils/Images/Phone.png';
 import Land from '../Utils/Images/Land.png';
+
 const Component1 = () => {
   return (
-    <div className='flex flex-col lg:flex-row   lg:mt-19 lg:mb-36 justify-between'>
-      {/* Text Box  */}
-    <div className=' mt-20.5 ml-5 cs:mt-25 lg:ml-82.5 mb-11.5 lg:mb-0 w-258px cs:size-140.5 lg:size-2/5 flex items-center md:justify-center '>
-  <div >
-    
-    {/* Start and Sell Box  */}
-    <div className='text-7xl font-helvetica font-bold  '>
-    <h1>Start today.</h1>
-    <h1 >Sell tomorrow.</h1>
-    </div>
-    
-    <p className="max-w-md mt-2  md:mt-6 mb-6 font-Inter font-normal leading-6 ">
-      You don’t need an agency to launch a 3D Showroom. Do it yourself. No coding or design experience necessary.
-    </p>
+    <div className='flex flex-col lg:flex-row lg:mt-19 lg:mb-36 justify-between'>
+      {/* Text Box */}
+      <div className='mt-20.5 ml-5 cs:ml-8 cs:mt-25 lg:ml-82.5 mb-11.5 lg:mb-0 w-258px cs:size-140.5 lg:size-2/5 flex items-center cs:justify-center '>
+        <div>
+          {/* Start and Sell Box */}
+          <div className='text-7xl font-helvetica font-bold'>
+            <h1>Start today.</h1>
+            <h1>Sell tomorrow.</h1>
+          </div>
 
-    {/* Input Fields */}
-    <div className="flex font-sans_pro   flex-wrap">
-    <input type="text" id="first_name" className="w-175px h-10.5 mr-4 lg:w-50 lg:h-12.5  bg-custom_formcolor  text-custom-landing-input pt-3.5 pl-4 pb-3  rounded-md  " placeholder="Full Name*" required/>
-    <input type="email" id="company_name" className=" w-175px h-10.5 lg:w-50 lg:h-12.5 bg-custom_formcolor text-custom-landing-input pt-3.5 pl-4 pb-3  rounded-md   " placeholder="Company Name*" required/>
-    </div>
-    {/* full box */}
-    <div className='flex flex-col'>
-    <input type="text" placeholder='Company Website*' className='w-358px h-10.5 lg:h-12.5 lg:w-104 bg-custom_formcolor text-custom-landing-input pt-3.5 pl-4 pb-3 pr-25 rounded-md   mt-4 mb-4 ' />
-    <button className="text-xs w-358px h-10.5 lg:w-104 lg:h-13  bg-custom_comp1_green   pl-32.5 pr-32.5 lg:pt-2 lg:pb-2 text-white rounded-lg lg:mr-3 ">
-      <h1 className='text-sm font-semibold'> Put me in line</h1>
-      <p className='text-xs font-normal'>No Commitment</p>
-    </button>
-    </div>
-    <p className='text-custom_alert_grey  mt-2'>* fields are mandatory</p>
-  </div>
-</div>
- 
+          <p className="max-w-md mt-2 md:mt-6 mb-6 font-Inter font-normal leading-6">
+            You don’t need an agency to launch a 3D Showroom. Do it yourself. No coding or design experience necessary.
+          </p>
 
- {/* Images  */}
- <div className='relative ml-25 md:ml-37.5'>
-  <img src={Land} className="w-full h-296px cs:h-full" alt="Land Image" />
-  <img src={Phone} className='absolute left-[-50px] bottom-[-60px]  h-259px cs:h-611px md:left-[-44] cs:bottom-[-200px]  ' alt="Phone Image" />
-  {/* top-32 instead of bottom-[-192px] */}
-  
-</div>
+          {/* Input Fields */}
+          <div className="flex font-sans_pro flex-wrap">
+            <input type="text" id="first_name" className="w-175px h-10.5 mr-4 lg:w-50 lg:h-12.5 bg-custom_formcolor text-custom-landing-input pt-3.5 pl-4 pb-3 rounded-md" placeholder="Full Name*" required />
+            <input type="email" id="company_name" className="w-175px h-10.5 lg:w-50 lg:h-12.5 bg-custom_formcolor text-custom-landing-input pt-3.5 pl-4 pb-3 rounded-md" placeholder="Company Name*" required />
+          </div>
 
+          {/* Full box */}
+          <div className='flex flex-col'>
+            <input type="text" placeholder='Company Website*' className='w-358px h-10.5 lg:h-12.5 lg:w-104 bg-custom_formcolor text-custom-landing-input pt-3.5 pl-4 pb-3 pr-25 rounded-md mt-4 mb-4' />
+            <button className="text-xs w-358px h-10.5 lg:w-104 lg:h-13 bg-custom_comp1_green pl-32.5 pr-32.5 lg:pt-2 lg:pb-2 text-white rounded-lg lg:mr-3">
+              <h1 className='text-sm font-semibold'> Put me in line</h1>
+              <p className='text-xs font-normal'>No Commitment</p>
+            </button>
+          </div>
+          
+          <p className='text-custom_alert_grey mt-2'>* fields are mandatory</p>
+        </div>
+      </div>
+
+      {/* Images */}
+      <div className='relative ml-25 md:ml-37.5'>
+        <img src={Land} className="w-full h-296px cs:h-full" alt="Land Image" />
+        <img src={Phone} className='absolute left-[-50px] bottom-[-60px] h-259px cs:h-611px md:left-[-44] cs:bottom-[-200px]' alt="Phone Image" />
+        {/* top-32 instead of bottom-[-192px] */}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Component1;
--- src/Components/Component2.js
+++ src/Components/Component2.js
@@ -1,50 +1,41 @@
-import React from 'react'
-import Showroom from '../Utils/Images/Showroom.png'
+import React from 'react';
+import Showroom from '../Utils/Images/Showroom.png';
+
 const Component2 = () => {
   return (
     <div className="bg-custom_nav_comp2_blue w-full mt-97px cs:mt-67.5">
-     {/* Upper part */}
-     <div className="flex flex-col lg:flex-row w-full h-full justify-between">
-
-      {/* text box */}
-      <div className="bg-custom_Black flex flex-col mt-10  h-full w-full lg:w-787px">
-        <div className='mt:13 ml-5 md:ml-83.5 md:mt-13 md:mr-20.5 flex flex-wrap '>
-        <h1 className='text-custom_White font-bold mt-13  text-2xl leading-9 font-helvetica  flex flex-wrap '>Give your online shoppers a better brand experience and level up your customer behaviour insights.</h1>
-       <div className='flex flex-col space-y-4 md:space-y-6 mt-4 md:mt-8 md:mb-16 mb-33px'>
-       <button className="text-xs font-sans_pro font-semibold  mt-6 w-max pl-21px pt-2 pr-4.5 pb-7px rounded-full text-custom_White border-2 border-custom_bordercolor ">Store Builder</button>
-       <button className="text-xs font-sans_pro font-semibold   w-max pl-21px pt-2 pr-4.5 pb-7px rounded-full text-custom_White border-2 border-custom_bordercolor">Customer Journey Visualizations </button>
-       <button className="text-xs font-sans_pro font-semibold   w-max pl-21px pt-2 pr-4.5 pb-7px rounded-full text-custom_White border-2 border-custom_bordercolor">A/B Testing</button>
-       <button className="text-xs  font-sans_pro font-semibold   w-max pl-21px pt-2 pr-4.5 pb-7px rounded-full text-custom_White border-2 border-custom_bordercolor">Amplified Brand</button>
-       <button className="text-xs  font-sans_pro font-semibold   w-max pl-21px pt-2 pr-4.5 pb-7px mb-25 rounded-full text-custom_White border-2 border-custom_bordercolor">Centralized Management Portal</button>
-       </div>
-       
+      {/* Upper part */}
+      <div className="flex flex-col lg:flex-row w-full h-full justify-between">
+        {/* Text box */}
+        <div className="bg-custom_Black flex flex-col mt-10 h-full w-full lg:w-787px">
+          <div className='mt-13 ml-5 md:ml-83.5 md:mt-13 md:mr-20.5 flex flex-wrap '>
+            <h1 className='text-custom_White font-bold mt-13 text-2xl leading-9 font-helvetica flex flex-wrap '>Give your online shoppers a better brand experience and level up your customer behavior insights.</h1>
+            <div className='flex flex-col space-y-4 md:space-y-6 mt-4 md:mt-8 md:mb-16 mb-33px'>
+              <button className="text-xs font-sans_pro font-semibold mt-6 w-max pl-21px pt-2 pr-4.5 pb-7px rounded-full text-custom_White border-2 border-custom_bordercolor">Store Builder</button>
+              <button className="text-xs font-sans_pro font-semibold w-max pl-21px pt-2 pr-4.5 pb-7px rounded-full text-custom_White border-2 border-custom_bordercolor">Customer Journey Visualizations</button>
+              <button className="text-xs font-sans_pro font-semibold w-max pl-21px pt-2 pr-4.5 pb-7px rounded-full text-custom_White border-2 border-custom_bordercolor">A/B Testing</button>
+              <button className="text-xs font-sans_pro font-semibold w-max pl-21px pt-2 pr-4.5 pb-7px rounded-full text-custom_White border-2 border-custom_bordercolor">Amplified Brand</button>
+              <button className="text-xs font-sans_pro font-semibold w-max pl-21px pt-2 pr-4.5 pb-7px mb-25 rounded-full text-custom_White border-2 border-custom_bordercolor">Centralized Management Portal</button>
+            </div>
+          </div>
         </div>
-       
+        {/* Image Box */}
+        <div className="relative bg-custom_nav_comp2_blue  ">
+          <img src={Showroom} alt="Showroom Store" className="w-full h-full" />
+          <p className="pl-5 md:pl-13 pt-49px pb-13 h-31 font-helvetica font-medium text-lg leading-6 absolute bottom-0 left-0 right-0 bg-custom_comp2_opaque_blue bg-opacity-80 text-white ">Drag-and-drop products to rearrange store based on data findings</p>
+        </div>
       </div>
-     
-     {/* Image Box */}
-     <div className="relative  bg-custom_nav_comp2_blue  ">
-  <img src={Showroom} alt="Showroom Store" className="w-full h-full" />
-  {/* <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center"> */}
-    <p className=" pl-5 md:pl-13 pt-49px pb-13  h-31 font-helvetica font-medium text-lg leading-6 absolute bottom-0 left-0 right-0  bg-custom_comp2_opaque_blue bg-opacity-80 text-white ">Drag-and-drop products to rearrange store based on data findings</p>
-  {/* </div> */}
-</div>
 
-
-     </div>
-
-     {/* Lower part */}
-     <div className='flex flex-col items-center justify-center'>
-  <h1 className="text-4xl mt-10 ml-77px mr-77px md:mt-119px md:ml-129.5md: md:mr-129 font-helvetica  text-center font-bold leading-10   text-custom_White">Are you ready to create your Showroom?</h1>
-  <div className='flex  justify-around font-semibold mt-33px mb-39px md:mb-30  gap-2'>
-    <button className='text-xs text-center  bg-white font-sans_pro mr-3 md:mr-5  pl-37px pr-37px pt-3.5 pb-13px rounded-full border-1 border-gray-700'>Get Early Access</button>
-    <button className='text-xs text-center   pl-37px pr-37px pt-3.5 pb-13px font-sans_pro rounded-full text-custom_White border-2 border-white-700'>Plans and Pricing</button>
-  </div>
-
-</div>
-
+      {/* Lower part */}
+      <div className='flex flex-col items-center justify-center'>
+        <h1 className="text-4xl mt-10 ml-77px mr-77px md:mt-119px md:ml-129.5 md:mr-129 font-helvetica text-center font-bold leading-10 text-custom_White">Are you ready to create your Showroom?</h1>
+        <div className='flex justify-around font-semibold mt-33px mb-39px md:mb-30 gap-2'>
+          <button className='text-xs text-center bg-white font-sans_pro mr-3 md:mr-5 pl-37px pr-37px pt-3.5 pb-13px rounded-full border-1 border-gray-700'>Get Early Access</button>
+          <button className='text-xs text-center pl-37px pr-37px pt-3.5 pb-13px font-sans_pro rounded-full text-custom_White border-2 border-white-700'>Plans and Pricing</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Component2
+export default Component2;
